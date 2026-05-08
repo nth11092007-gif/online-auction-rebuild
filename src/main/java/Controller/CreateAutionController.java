c6cccccc079972e0168f5b97581bb522d487eeed
phuynguyen162-hub <phuynguyen162@gmail.com>
2026-05-08 13:16:24 +0700

    Hthanh Home, ItemCard, CreateAution

--- src/main/java/Controller/CreateAutionController.java
+++ src/main/java/Controller/CreateAutionController.java
@@ -1,5 +1,7 @@
 package Controller;
 
+import dao.AuctionSessionDAO;
+import dao.AuctionSessionDAOImpl;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -8,7 +10,7 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 public class CreateAutionController {
-
+    AuctionSessionDAO auctionSessionDAO = new AuctionSessionDAOImpl();
     @FXML
     private Button btnClearForm;
 
@@ -16,7 +18,7 @@ public class CreateAutionController {
     private Button btnCreateAution;
 
     @FXML
-    private ComboBox<?> cbItemType;
+    private ComboBox<String> cbItemType;
 
     @FXML
     private TextArea txtDescription;
@@ -35,12 +37,26 @@ public class CreateAutionController {
 
     @FXML
     void HandleClearForm(ActionEvent event) {
-
+        txtItemName.clear();
+        txtStartingPrice.clear();
+        txtIncrementStep.clear();
+        txtOpenDays.clear();
+        txtDescription.clear();
+        cbItemType.getSelectionModel().clearSelection();
     }
 
     @FXML
     void HandleCreatAution(ActionEvent event) {
-
+        String itemName = txtItemName.getText();
+        double startPrice = Double.parseDouble(txtStartingPrice.getText());
+        double stepPrice = Double.parseDouble(txtIncrementStep.getText());
+        int openDays = Integer.parseInt(txtOpenDays.getText());
+        String description = txtDescription.getText();
+    }
+    @FXML
+    public void initialize() {
+        // Thêm các loại vật phẩm vào ComboBox
+        cbItemType.getItems().addAll("Electronics", "Arts", "Vehicles");
     }
 
 }
